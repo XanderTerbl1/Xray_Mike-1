cc2500d3caba648ad285cc2bb447ba65e407a035
XanderTerbl1 <xanderterbl268@gmail.com>
2017-05-29 06:52:08 +0200

    Updated GUI
    I updated the Master GUI

--- frm_Park_proj.dpr
+++ frm_Park_proj.dpr
@@ -14,10 +14,10 @@ uses
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TForm4, Form4);
   Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm3, Form3);
-  Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
